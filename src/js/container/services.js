b2c277b011350b2cc62806ad8b8062e241948597
yangyao <873241789@qq.com>
2019-01-30 18:20:11 +0800

    分离 产品介绍js

---
+++ src/js/container/services.js
@@ -0,0 +1,154 @@
+/**
+ * Copyright: Copyright (C) 2018 sitb.software,All Rights Reserved
+ * author: yangyao(873241789@qq.com)
+ * date: 2019/1/30
+ */
+
+// 产品介绍配置
+var productConfig = [{
+    moduleSubtitle: '产品介绍',
+    moduleTitle: '聚合支付系统',
+    productSubtitle: '聚合支付系统',
+    productTitle: '为商户提供覆盖主流支付渠道的全场景支付服务',
+    coreConfig: [{
+        title: '支付方式',
+        subtitle: '立码支付、智能POS机、公众号支付、API模式、SDK收银台、WEB收银台.',
+        src: 'src/img/core-img/trophy.png'
+    }, {
+        title: '业务优势',
+        subtitle: '一点接入、会员管理、全场景覆盖、统一结算、营销管理、卡券管理核销、T+0结算（优质商户）、结算款理财（优质商户）.',
+        src: 'src/img/core-img/atomic.png'
+    }, {
+        title: '应用场景(线上支付)',
+        subtitle: '商业场景（用户通过网络购买商品或在线预订）适用行业（互联网商城、电子客票交易平台、火车票/汽车票/船票/酒店在线预订、机票代理网站）相应产品（SDK收银台、WEB收银台、公众号支付).',
+        src: 'src/img/core-img/presentation.png'
+    }, {
+        title: '应用场景(线下支付)',
+        subtitle: '商业场景（用户在线下商铺、超市等地方消费，使用移动手持设备结账） 适用行业（食品、美容用品、书籍、家电、家居建材、服饰零售、母婴商城）相应产品（立码支付、智能POS机）.',
+        src: 'src/img/core-img/safebox.png'
+    }]
+}, {
+    moduleSubtitle: '产品介绍',
+    moduleTitle: '互联网支付',
+    productSubtitle: '互联网支付',
+    productTitle: '基于PC平台的网上系统对接和部署，使商户能够通过银行卡和数字钱包在互联网上完成支付交易',
+    coreConfig: [{
+        title: '支付方式',
+        subtitle: '立码支付、智能POS机、公众号支付、API模式、SDK收银台、WEB收银台.',
+        src: 'src/img/core-img/trophy.png'
+    }, {
+        title: '业务优势',
+        subtitle: '一点接入、会员管理、全场景覆盖、统一结算、营销管理、卡券管理核销、T+0结算（优质商户）、结算款理财（优质商户）.',
+        src: 'src/img/core-img/atomic.png'
+    }, {
+        title: '应用场景(线上支付)',
+        subtitle: '商业场景（用户通过网络购买商品或在线预订）适用行业（互联网商城、电子客票交易平台、火车票/汽车票/船票/酒店在线预订、机票代理网站）相应产品（SDK收银台、WEB收银台、公众号支付).',
+        src: 'src/img/core-img/presentation.png'
+    }, {
+        title: '应用场景(线下支付)',
+        subtitle: '商业场景（用户在线下商铺、超市等地方消费，使用移动手持设备结账） 适用行业（食品、美容用品、书籍、家电、家居建材、服饰零售、母婴商城）相应产品（立码支付、智能POS机）.',
+        src: 'src/img/core-img/safebox.png'
+    }]
+}, {
+    moduleSubtitle: '产品介绍',
+    moduleTitle: '数据查询',
+    productSubtitle: '数据查询',
+    productTitle: '基于PC平台的网上系统对接和部署，使商户能够通过银行卡和数字钱包在互联网上完成支付交易',
+    coreConfig: [{
+        title: '支付方式',
+        subtitle: '立码支付、智能POS机、公众号支付、API模式、SDK收银台、WEB收银台.',
+        src: 'src/img/core-img/trophy.png'
+    }, {
+        title: '业务优势',
+        subtitle: '一点接入、会员管理、全场景覆盖、统一结算、营销管理、卡券管理核销、T+0结算（优质商户）、结算款理财（优质商户）.',
+        src: 'src/img/core-img/atomic.png'
+    }, {
+        title: '应用场景(线上支付)',
+        subtitle: '商业场景（用户通过网络购买商品或在线预订）适用行业（互联网商城、电子客票交易平台、火车票/汽车票/船票/酒店在线预订、机票代理网站）相应产品（SDK收银台、WEB收银台、公众号支付).',
+        src: 'src/img/core-img/presentation.png'
+    }, {
+        title: '应用场景(线下支付)',
+        subtitle: '商业场景（用户在线下商铺、超市等地方消费，使用移动手持设备结账） 适用行业（食品、美容用品、书籍、家电、家居建材、服饰零售、母婴商城）相应产品（立码支付、智能POS机）.',
+        src: 'src/img/core-img/safebox.png'
+    }]
+}, {
+    moduleSubtitle: '产品介绍',
+    moduleTitle: '会员账户系统',
+    productSubtitle: '会员账户系统',
+    productTitle: '基于PC平台的网上系统对接和部署，使商户能够通过银行卡和数字钱包在互联网上完成支付交易',
+    coreConfig: [{
+        title: '支付方式',
+        subtitle: '立码支付、智能POS机、公众号支付、API模式、SDK收银台、WEB收银台.',
+        src: 'src/img/core-img/trophy.png'
+    }, {
+        title: '业务优势',
+        subtitle: '一点接入、会员管理、全场景覆盖、统一结算、营销管理、卡券管理核销、T+0结算（优质商户）、结算款理财（优质商户）.',
+        src: 'src/img/core-img/atomic.png'
+    }, {
+        title: '应用场景(线上支付)',
+        subtitle: '商业场景（用户通过网络购买商品或在线预订）适用行业（互联网商城、电子客票交易平台、火车票/汽车票/船票/酒店在线预订、机票代理网站）相应产品（SDK收银台、WEB收银台、公众号支付).',
+        src: 'src/img/core-img/presentation.png'
+    }, {
+        title: '应用场景(线下支付)',
+        subtitle: '商业场景（用户在线下商铺、超市等地方消费，使用移动手持设备结账） 适用行业（食品、美容用品、书籍、家电、家居建材、服饰零售、母婴商城）相应产品（立码支付、智能POS机）.',
+        src: 'src/img/core-img/safebox.png'
+    }]
+}, {
+    moduleSubtitle: '产品介绍',
+    moduleTitle: '预付费卡系统',
+    productSubtitle: '预付费卡系统',
+    productTitle: '基于PC平台的网上系统对接和部署，使商户能够通过银行卡和数字钱包在互联网上完成支付交易',
+    coreConfig: [{
+        title: '支付方式',
+        subtitle: '立码支付、智能POS机、公众号支付、API模式、SDK收银台、WEB收银台.',
+        src: 'src/img/core-img/trophy.png'
+    }, {
+        title: '业务优势',
+        subtitle: '一点接入、会员管理、全场景覆盖、统一结算、营销管理、卡券管理核销、T+0结算（优质商户）、结算款理财（优质商户）.',
+        src: 'src/img/core-img/atomic.png'
+    }, {
+        title: '应用场景(线上支付)',
+        subtitle: '商业场景（用户通过网络购买商品或在线预订）适用行业（互联网商城、电子客票交易平台、火车票/汽车票/船票/酒店在线预订、机票代理网站）相应产品（SDK收银台、WEB收银台、公众号支付).',
+        src: 'src/img/core-img/presentation.png'
+    }, {
+        title: '应用场景(线下支付)',
+        subtitle: '商业场景（用户在线下商铺、超市等地方消费，使用移动手持设备结账） 适用行业（食品、美容用品、书籍、家电、家居建材、服饰零售、母婴商城）相应产品（立码支付、智能POS机）.',
+        src: 'src/img/core-img/safebox.png'
+    }]
+}, {
+    moduleSubtitle: '产品介绍',
+    moduleTitle: '代收代付系统',
+    productSubtitle: '代收代付系统',
+    productTitle: '基于PC平台的网上系统对接和部署，使商户能够通过银行卡和数字钱包在互联网上完成支付交易',
+    coreConfig: [{
+        title: '支付方式',
+        subtitle: '立码支付、智能POS机、公众号支付、API模式、SDK收银台、WEB收银台.',
+        src: 'src/img/core-img/trophy.png'
+    }, {
+        title: '业务优势',
+        subtitle: '一点接入、会员管理、全场景覆盖、统一结算、营销管理、卡券管理核销、T+0结算（优质商户）、结算款理财（优质商户）.',
+        src: 'src/img/core-img/atomic.png'
+    }, {
+        title: '应用场景(线上支付)',
+        subtitle: '商业场景（用户通过网络购买商品或在线预订）适用行业（互联网商城、电子客票交易平台、火车票/汽车票/船票/酒店在线预订、机票代理网站）相应产品（SDK收银台、WEB收银台、公众号支付).',
+        src: 'src/img/core-img/presentation.png'
+    }, {
+        title: '应用场景(线下支付)',
+        subtitle: '商业场景（用户在线下商铺、超市等地方消费，使用移动手持设备结账） 适用行业（食品、美容用品、书籍、家电、家居建材、服饰零售、母婴商城）相应产品（立码支付、智能POS机）.',
+        src: 'src/img/core-img/safebox.png'
+    }]
+}];
+
+$('#header').html(MenuTemplate());
+$('#footer').html(FootNavigationTemplate());
+
+$('#servicesProductIntroduction').html(ProductIntroductionTemplate(productConfig));
+$('#servicesProductIntroduction .single-more-service-area').css({"padding": "20px"});
+// 页脚banner配置
+var servicesFootBannerConfig = {
+    title: '你还在犹豫什么?',
+    subtitle: '立即注册',
+    url: 'src/img/bg-img/sub-1.jpg',
+    buttonName: '立即注册，享受豪华套餐'
+};
+$('#servicesFootBanner').html(FootBannerTemplate(servicesFootBannerConfig));
